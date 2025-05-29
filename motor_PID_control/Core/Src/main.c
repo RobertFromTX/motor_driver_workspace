52172187938d259fe1b2fac3cdf643f0843bdd9e
RobertFromTX <robc.btx@gmail.com>
2025-05-29 00:11:47 -0500

    implemented wrap around for encoder to fix error and setpoint

--- motor_PID_control/Core/Src/main.c
+++ motor_PID_control/Core/Src/main.c
@@ -156,7 +156,8 @@ int main(void)
 
 	pid_controller motor1_controller;
 	initialize_PID(&motor1_controller, 0);
-	set_gains_PID(&motor1_controller, 6, 30, 10);
+	// set_gains_PID(&motor1_controller, 6, 30, 10); //RED MOTOR
+	set_gains_PID(&motor1_controller, 12, 600, .02); //YELLOW MOTOR
 	/* USER CODE END 2 */
 
 	/* Infinite loop */
@@ -168,7 +169,7 @@ int main(void)
 		while (!position_ready);
 		position_ready = 0;	//set flag back to 0
 		uint16_t motor_position1 = motor1_pos[0];
-		update_PID(&motor1_controller, motor_position1, 15);
+		update_PID(&motor1_controller, motor_position1, 270);
 		update_motor_input((int16_t) motor1_controller.total_out, &active_buffer, &inactive_buffer);
 		/* USER CODE END WHILE */
 
@@ -473,10 +474,10 @@ void initialize_PID(pid_controller *controller, uint16_t updated_measured_pos)
 {
 
 	controller->Ts = 0.0001; //equates to 10kHz
-	controller->tau = 10;
+	controller->tau = .001;
 
-	controller->out_max = 600;
-	controller->out_min = -600;
+	controller->out_max = 800;
+	controller->out_min = -800;
 
 	controller->proportional_gain = 0;
 	controller->integral_gain = 0;
@@ -499,29 +500,34 @@ void set_gains_PID(pid_controller *controller, float Kp, float Ki, float Kd)
 void update_PID(pid_controller *controller, uint16_t updated_measured_pos, uint16_t set_point)
 {
 	int16_t adjusted_measured_pos = updated_measured_pos;
-	int16_t adjusted_prev_pos = controller->measured_pos;
 
 	int16_t updated_error = set_point - updated_measured_pos;
 
-	if (updated_measured_pos > set_point + 180 && set_point < 90)
+	//this block makes sure that if the setpoint is near boundaries (0 or 359 degrees), can still approach the setpoint
+	//from the direction that has the angle measurement spike from 0 to 359 degrees or 359 to 0 degrees
+	//this is done by adjusting the error term
+	if (updated_measured_pos > set_point + 180 && set_point < 180)
 	{
-		adjusted_measured_pos = adjusted_measured_pos - 361;
+		adjusted_measured_pos = adjusted_measured_pos - 360;
 	}
-	else if (updated_measured_pos > set_point - 180 && set_point > 300)
+	else if (updated_measured_pos < set_point - 180 && set_point >= 180)
 	{
-		adjusted_measured_pos = adjusted_measured_pos + 361;
+		adjusted_measured_pos = adjusted_measured_pos + 360;
 	}
-	if (controller->measured_pos > set_point + 180 && set_point < 90)
-	{
-		adjusted_prev_pos = adjusted_prev_pos - 361;
-	}
-	else if (controller->measured_pos > set_point - 180 && set_point > 300)
-	{
-		adjusted_prev_pos = adjusted_prev_pos + 361;
-	}
-	int16_t position_difference = adjusted_measured_pos - adjusted_prev_pos;
-
 	updated_error = set_point - adjusted_measured_pos;
+
+	//calculate difference for derivative term, but need to account for when motor goes from 359->0 and 0->359
+	//make sure that when it goes 359->0, the difference is 1, and 0->359 is -1
+	int16_t position_difference = updated_measured_pos - controller->measured_pos;
+	if (position_difference > 300) //when it goes from 0 to 359
+	{
+		position_difference = position_difference - 360;
+	}
+	else if (position_difference < -300) //when it goes from 359 to 0
+	{
+		position_difference = position_difference + 360;
+	}
+
 	//updated the outputs of the P, I, and D components of the controller
 	controller->proportional_out = controller->proportional_gain * updated_error;
 	controller->integral_out = controller->integral_gain * controller->Ts * (updated_error + controller->error) / 2.0 + controller->integral_out;
@@ -534,7 +540,7 @@ void update_PID(pid_controller *controller, uint16_t updated_measured_pos, uint1
 	//determine integrator limits
 	if (controller->out_max > controller->proportional_out)
 	{
-		integral_max = controller->out_max - controller->proportional_out;
+		integral_max = controller->out_max - controller->proportional_out + controller->derivative_out; //see total_out comment to see why adding derivative term instead of subtracting here
 	}
 	else
 	{
@@ -542,7 +548,7 @@ void update_PID(pid_controller *controller, uint16_t updated_measured_pos, uint1
 	}
 	if (controller->out_min < controller->proportional_out)
 	{
-		integral_min = controller->out_min - controller->proportional_out;
+		integral_min = controller->out_min - controller->proportional_out + controller->derivative_out; //see total_out comment to see why adding derivative term instead of subtracting here
 	}
 	else
 	{
@@ -555,10 +561,14 @@ void update_PID(pid_controller *controller, uint16_t updated_measured_pos, uint1
 	{
 		absval_error = -1 * absval_error;
 	}
+
 	if (absval_error < 30) //limit integrator even more once closer to desired angle.
 	{
-		integral_max = 220;
-		integral_min = -220;
+//		integral_max = 220; //RED MOTOR
+//		integral_min = -220;
+
+		integral_max = 340; //YELLOW MOTOR
+		integral_min = -340;
 	}
 	//clamping of integrator
 	if (controller->integral_out > integral_max)
@@ -571,7 +581,7 @@ void update_PID(pid_controller *controller, uint16_t updated_measured_pos, uint1
 	}
 
 	//compute total output of controller
-	controller->total_out = controller->proportional_out + controller->integral_out + controller->derivative_out;
+	controller->total_out = controller->proportional_out + controller->integral_out - controller->derivative_out; //note negative sign on derivative term, this is correct since it is on the feedback loop
 
 	//limit total output of controller
 	if (controller->total_out > controller->out_max)
